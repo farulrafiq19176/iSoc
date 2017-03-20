f7a393c40e657f3c997657c32e7870f36375b3d8
farulrafiq19176 <farulrafiq_19176@utp.edu.my>
2017-03-20 23:01:16 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>iSoc - Order Management System</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+  		<div class="container-fluid">
+    		<div class="navbar-header">
+      			<a class="navbar-brand" href="#"><img src="Mcdonalds-95-logo.png"></a>
+    		</div>
+
+    		<div class="collapse navbar-collapse" id="myNavbar">
+      			<ul class="nav navbar-nav navbar-right">
+        			<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        			<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      			</ul>
+    		</div>
+  		</div>
+	</nav>
+
+	<nav>
+	<div id="order" class="container text-center">
+		<h3>New Way of Ordering Fast Food</h3>
+		<p><em>We love you!</em></p>
+		<p>We have created the best for you! Enjoy it by clicking.. </p>
+		<button type="button" class="btn btn-primary btn-lg">ORDER NOW</button>
+	</nav>
+
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  	
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	      <img src="iklan1.jpg" alt="iklan1">
+	    </div>
+
+	    <div class="item">
+	      <img src="iklan3.jpg" alt="iklan3">
+	    </div>
+
+	  <!-- Left and right controls -->
+	  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+	<!-- Footer -->
+	<footer class="text-center">
+	  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+	    <span class="glyphicon glyphicon-chevron-up"></span>
+	  </a><br><br>
+	  <p>Managed by <a href="#" data-toggle="tooltip" title="Visit mavericks">www.mavericks.com</a></p> 
+
+	</footer>
+
+</body>
+</html>
